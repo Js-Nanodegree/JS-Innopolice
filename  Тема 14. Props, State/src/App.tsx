0db8eb0e30a2dba218bbf9ae7b2b6a9d04089a9d
Yakimov Vyacheslav <js-nanodegree@outlook.com>
2021-11-07 11:00:45 +0300

    fix screen

---  Тема 14. Props, State/src/App.tsx
+++  Тема 14. Props, State/src/App.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 import styled from 'styled-components';
 
+import Table from './Table';
+
+
 function App() {
   return (
     <Container>
+      <Table/>
     </Container>
   );
 }
@@ -13,6 +17,7 @@ export default App;
 
 
 const Container = styled.div`
-background-color:red;
-
+width:100%;
+height:100%;
+display: flex;
 `;
