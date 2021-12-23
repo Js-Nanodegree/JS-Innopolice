0d768c5f1eb9a5218d07640cea2138af4570df6e
Yakimov Vyacheslav <js-nanodegree@outlook.com>
2021-12-23 20:37:33 +0300

    fix screen

---
+++ Аттестация работа 2/src/resolver/Query/getAllAppealChannel.ts
@@ -0,0 +1,31 @@
+import * as iType from "../interface";
+import { searchRefChannel } from "./getChannel";
+import { resultString } from "./resultString";
+
+export async function getAllAppealChannel({ appeal, client, workshop, workspaceType }: any) {
+  if (workspaceType === iType.ENUM_TYPE.APPEAL && appeal) {
+    const commentsRef = searchRefChannel(
+      "appealId",
+      appeal,
+      process.env.ROUTE_CHANNEL||""
+    );
+    return resultString(commentsRef)
+  }
+  if (workspaceType === iType.ENUM_TYPE.CLIENT && client?.id) {
+    const commentsRef = searchRefChannel(
+      'clientId',
+      client?.id,
+      process.env.ROUTE_CHANNEL||""
+    );
+    return resultString(commentsRef)
+  }
+  if (workspaceType === iType.ENUM_TYPE.GARAGE && workshop?.[0]?.id) {
+    const commentsRef = searchRefChannel(
+      `workshop_${workshop[0].id}`,
+      true,
+      process.env.ROUTE_CHANNEL||""
+    );
+    return resultString(commentsRef)
+  }
+  throw new Error('params not this valid');
+}
